6801227cc3a3dd753e54c98fd1bb060f755b85d0
Nhan <natenguyen51294@gmail.com>
2020-08-07 10:45:00 -0500

    ex2 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,50 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+    def __str__(self):
+        return f"From {self.source} to {self.destination}"
+
+
+# The tickets are messed up but we're still able to figure out a way
+# to sort them. The very first ticket would be NONE -> 'something'.
+# We can save the key:value as source:destination of tickets to a dict.
+# We can add the very first destination to our arr .Now we can start
+# looping to sort the tickets.
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    route = []
+
+    for i in tickets:
+        k, v = i.source, i.destination
+        cache[k] = v
+
+    route.append(cache['NONE'])
+    cur = cache[route[0]]
+    count = 1
+
+    while count < length:
+        # while cur != 'NONE':
+        route.append(cur)
+        cur = cache[cur]
+        count += 1
 
     return route
+
+
+tickets = [
+    Ticket("PIT", "ORD"),
+    Ticket("XNA", "CID"),
+    Ticket("SFO", "BHM"),
+    Ticket("FLG", "XNA"),
+    Ticket("NONE", "LAX"),
+    Ticket("LAX", "SFO"),
+    Ticket("CID", "SLC"),
+    Ticket("ORD", "NONE"),
+    Ticket("SLC", "PIT"),
+    Ticket("BHM", "FLG")
+]
+
+length = 10
+
+reconstruct_trip(tickets, length)
